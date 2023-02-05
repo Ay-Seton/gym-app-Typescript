bab7f87a35f636dff48d1326adb878078217565c
Ayoola Abidoye <Ayoola.abidoye@gmail.com>
2023-02-06 00:55:57 +0100

    Update overflow issue

--- src/scenes/home/index.tsx
+++ src/scenes/home/index.tsx
@@ -1,42 +1,39 @@
-import { SelectedPage } from '@/shared/types'
-import useMediaQuery from '@/hooks/useMediaQueries';
-import ActionButton from '@/shared/ActionButton';
+import { SelectedPage } from "@/assets/shared/type";
+import AnchorLink from "react-anchor-link-smooth-scroll";
+import ActionButton from "@/assets/shared/ActionButton";
 import HomePageText from "@/assets/HomePageText.png";
 import HomePageGraphic from "@/assets/HomePageGraphic.png";
 import SponsorRedBull from "@/assets/SponsorRedBull.png";
 import SponsorForbes from "@/assets/SponsorForbes.png";
 import SponsorFortune from "@/assets/SponsorFortune.png";
-import AnchorLink from 'react-anchor-link-smooth-scroll';
-import { motion } from 'framer-motion';
-
-
+import useMediaQuery from "@/assets/hooks/useMediaQuery";
+import { motion } from "framer-motion";
 
 type Props = {
-    setSelectedPage: (value: SelectedPage) => void;
-}
+  setSelectedPage: (value: SelectedPage) => void;
+};
 
-const Home = ({ setSelectedPage}: Props) =>{
+const Home = ({ setSelectedPage }: Props) => {
   const isAboveMediumScreens = useMediaQuery("(min-width:1060px)");
 
   return (
-    <section
-      id="home"
-      className="gap-16 bg-gray-20 py-10 pb-10 md:h-full md:pb-0"
-    >
+    <section id="home" className="bg-gray-20 py-10 md:h-full md:pb-0">
       {/* IMAGE AND MAIN HEADER */}
       <motion.div
         className="mx-auto w-5/6 items-center justify-center md:flex md:h-5/6"
-        onViewportEnter={() => setSelectedPage(SelectedPage.Home)}
+        onViewportEnter={() => {
+          setSelectedPage(SelectedPage.Home);
+        }}
       >
         {/* MAIN HEADER */}
-        <div className="z-10 mt-32 md:basis-3/5">
+        <div className="mt-12 md:basis-3/5">
           {/* HEADINGS */}
           <motion.div
-            className="md:-mt-20"
+            className=""
             initial="hidden"
             whileInView="visible"
             viewport={{ once: true, amount: 0.5 }}
-            transition={{ delay: 0.2, duration: 0.5 }}
+            transition={{ duration: 0.5 }}
             variants={{
               hidden: { opacity: 0, x: -50 },
               visible: { opacity: 1, x: 0 },
@@ -48,59 +45,59 @@ const Home = ({ setSelectedPage}: Props) =>{
               </div>
             </div>
             <p className="mt-8 text-sm">
-              Unrivaled Gym. Unparalleled Training Fitness Classes. World class
+              Unrivaled Gym. Unparalleled Training Fitness Classes. World Class
               Studios to get the Body Shapes That you Dream of.. Get Your Dream
               Body Now.
             </p>
           </motion.div>
-        </div>
-        {/* ACTIONS */}
-        <motion.div
-          className="mt-8 flex items-center gap-8"
-          initial="hidden"
-          whileInView="visible"
-          viewport={{ once: true, amount: 0.5 }}
-          transition={{ delay: 0.2, duration: 0.5 }}
-          variants={{
-            hidden: { opacity: 0, x: -50 },
-            visible: { opacity: 1, x: 0 },
-          }}
-        >
-          <ActionButton setSelectedPage={setSelectedPage}>
-            Join Now
-          </ActionButton>
-          <AnchorLink
-            className="text-sm font-bold text-primary-500 underline hover:text-secondary-500"
-            onClick={() => setSelectedPage(SelectedPage.ContactUs)}
-            href={`#${SelectedPage.ContactUs}`}
+          {/* ACTIONS */}
+          <motion.div
+            className="mt-8 flex items-center gap-8"
+            initial="hidden"
+            whileInView="visible"
+            viewport={{ once: true, amount: 0.5 }}
+            transition={{ delay: 0.5, duration: 0.5 }}
+            variants={{
+              hidden: { opacity: 0, x: -50 },
+              visible: { opacity: 1, x: 0 },
+            }}
           >
-            <p> Learn More</p>
-          </AnchorLink>
-        </motion.div>
+            <ActionButton setSelectedPage={setSelectedPage}>
+              Join Now
+            </ActionButton>
+            <AnchorLink
+              className="text-sm font-bold text-primary-500 underline"
+              onClick={() => {
+                setSelectedPage(SelectedPage.ContactUs);
+              }}
+              href={`#${SelectedPage.ContactUs}`}
+            >
+              <p>Learn More</p>
+            </AnchorLink>
+          </motion.div>
+        </div>
+        {/* IMAGE */}
+        <div
+          className="justify-center md:z-10 md:basis-3/5
+             md:mt-16 md:ml-40"
+        >
+          <img alt="home-page-graphic" src={HomePageGraphic} />
+        </div>
       </motion.div>
-      {/* IMAGE */}
-      <div
-        className="flex basis-3/5 justify-center md:z-10
-          md:ml-40 md:mt-16 md:justify-items-end"
-      >
-        <img alt="home-pageGraphic" src={HomePageGraphic} />
-      </div>
-
-      {/* SPONSORS */}
+      {/* SPONSOR  */}
       {isAboveMediumScreens && (
-        <div className="h-[150px] w-full bg-primary-100 py-10">
+        <div className="flex h-[150px] w-full bg-primary-100 py-10">
           <div className="mx-auto w-5/6">
             <div className="flex w-3/5 items-center justify-between gap-8">
-              <img alt="redbull-sponsor" src={SponsorRedBull} />
               <img alt="forbes-sponsor" src={SponsorForbes} />
               <img alt="fortune-sponsor" src={SponsorFortune} />
+              <img alt="redbull-sponsor" src={SponsorRedBull} />
             </div>
           </div>
         </div>
       )}
     </section>
   );
-}
+};
 
-
-export default Home
+export default Home;
